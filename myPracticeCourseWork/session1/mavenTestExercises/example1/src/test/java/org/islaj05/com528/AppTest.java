348429bac30ad4568360b1b82cbab38bf200dfe7
5islaj05 <5islaj05@solent.ac.uk>
2021-10-05 22:22:00 +0100

    Session 1

---
+++ myPracticeCourseWork/session1/mavenTestExercises/example1/src/test/java/org/islaj05/com528/AppTest.java
@@ -0,0 +1,20 @@
+package org.islaj05.com528;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
